100619cb1be71df845cd06d24fe74d466786f206
SecretKeeper <secretk33per@icloud.com>
2022-07-13 01:39:38 +0430

    add bottom border bot nav

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -3,10 +3,12 @@ import Logo from "./logo";
 import MenuC from "./menu";
 
 const Navigation = (): JSX.Element => (
-	<div className="rtl container flex items-center mx-auto py-5">
-		<Logo />
-		<MenuC />
-		<AuthActions />
+	<div className="border-b">
+		<div className="rtl container flex items-center mx-auto py-5">
+			<Logo />
+			<MenuC />
+			<AuthActions />
+		</div>
 	</div>
 );
 
