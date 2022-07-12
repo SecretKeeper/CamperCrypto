e7133cebcf5b4d507e810a96e5aa049b4cdbbe54
SecretKeeper <secretk33per@icloud.com>
2022-07-13 01:17:50 +0430

    added typography

--- utilities/muiTheme.ts
+++ utilities/muiTheme.ts
@@ -1,9 +1,19 @@
 import { createTheme } from "@mui/material/styles";
 
 const MUITheme = createTheme({
+	typography: {
+		fontFamily: [
+			"-apple-system",
+			"Vazir",
+			"Roboto",
+			'"Helvetica Neue"',
+			"Arial",
+			"sans-serif",
+		].join(","),
+	},
 	palette: {
 		primary: {
-			main: "#2354e7",
+			main: "#2354e7 !important",
 		},
 		secondary: {
 			main: "#f44336",
